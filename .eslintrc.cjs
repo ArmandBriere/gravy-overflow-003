65aeca84ca73ea8c5db16949d80a7556c7f79eb0
Armand Briere <armand.123@live.fr>
2024-06-19 19:13:56 -0400

    stuff

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,25 +1,27 @@
+let toto;
+toto = process.env.FLAG_GRAVY_OVERFLOW_L0_GRAVY;
+console.log(toto.substr(0, 3));
+console.log(toto.substr(3));
+
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
     },
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
-    }
-}
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {},
+};
